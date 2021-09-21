dd0e651ec9d8825fcba0310a83726d5e9a724027
swetant <70318943+swetant@users.noreply.github.com>
2021-09-21 14:18:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app=express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+http .listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`);
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+const io=require('socket.io')(http)
+const users={};
+io.on("connection",socket=>{
+    console.log("connected...")
+    socket.on('new-user-joined',name=>{
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined', name);
+    });
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]});
+    })
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+
+    })
+})
